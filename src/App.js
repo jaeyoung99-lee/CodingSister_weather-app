902a685e998677a31bb98bd376a3f666ea95c86e
jaeyoung99-lee <leejy0715@naver.com>
2024-08-17 08:07:47 +0900

    Implementing Input Capabilities

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import WeatherBox from "./component/WeatherBox";
 import WeatherButton from "./component/WeatherButton";
 import ClipLoader from "react-spinners/ClipLoader";
+import { Button, Modal } from "react-bootstrap";
 
 // 1. 앱이 실행되자마자 현재 위치 기반의 날씨가 보인다
 // 2. 날씨 정보에는 도시, 섭씨, 화씨, 날씨 상태
@@ -16,9 +17,13 @@ const API_KEY = "c1d4978c51d55df7b0df6aa7961250d9";
 
 function App() {
   const [weather, setWeather] = useState(null);
-  const cities = ["Seoul", "Gyeonggi-do", "Busan", "New York"];
   const [city, setCity] = useState("");
   const [loading, setLoading] = useState(false); // 로딩 스피너
+  const [searchCity, setSearchCity] = useState("");
+  const [showError, setShowError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const cities = ["Seoul", "Gyeonggi-do", "Busan", "New York"];
 
   const getCurrentLocation = useCallback(() => {
     navigator.geolocation.getCurrentPosition((position) => {
@@ -32,22 +37,54 @@ function App() {
   const getWeatherByCurrentLocation = async (lat, lon) => {
     let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric&lang=kr`;
     setLoading(true);
-    let response = await fetch(url);
-    let data = await response.json();
-    console.log("data :", data);
-    setWeather(data);
-    setLoading(false);
+    try {
+      let response = await fetch(url);
+      let data = await response.json();
+      console.log("data :", data);
+      setWeather(data);
+    } catch (error) {
+      setErrorMessage(
+        "현재 위치의 날씨 정보를 가져올 수 없습니다." + error.message
+      );
+      setShowError(true);
+    } finally {
+      setLoading(false);
+    }
   };
 
   const getWeatherByCity = useCallback(async () => {
     let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric&lang=kr`;
     setLoading(true);
-    let response = await fetch(url);
-    let cityData = await response.json();
-    console.log("city data :", cityData);
-    setWeather(cityData);
-    setLoading(false);
+    try {
+      let response = await fetch(url);
+      let cityData = await response.json();
+      console.log("city data :", cityData);
+      setWeather(cityData);
+    } catch (error) {
+      setErrorMessage(
+        "입력한 도시의 날씨 정보를 찾을 수 없습니다." + error.message
+      );
+      setShowError(true);
+    } finally {
+      setLoading(false);
+    }
   }, [city]);
+
+  const userSearchCity = () => {
+    setCity(searchCity);
+    setSearchCity("");
+  };
+
+  const EnterKeyUp = (e) => {
+    if (e.key === "Enter") {
+      userSearchCity();
+    }
+  };
+
+  const ErrorClose = () => {
+    setShowError(false);
+    getCurrentLocation();
+  };
 
   useEffect(() => {
     if (city === "") {
@@ -62,6 +99,23 @@ function App() {
 
   return (
     <div>
+      <div className="search-box">
+        <input
+          type="text"
+          value={searchCity}
+          onChange={(e) => {
+            setSearchCity(e.target.value);
+          }}
+          onKeyUp={EnterKeyUp}
+          placeholder="Enter city name(Language : English)"
+        />
+        <button onClick={userSearchCity}>
+          <img
+            src={`https://cdn-icons-png.flaticon.com/512/3593/3593144.png`}
+            alt="Search Icon"
+          />
+        </button>
+      </div>
       {loading ? (
         <div className="container">
           <ClipLoader color="rgb(255, 165, 0)" loading={loading} size={150} />
@@ -72,6 +126,18 @@ function App() {
           <WeatherButton cities={cities} setCity={setCity} currentCity={city} />
         </div>
       )}
+
+      <Modal show={showError} onHide={() => setShowError(false)}>
+        <Modal.Header closeButton>
+          <Modal.Title>Error</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>{errorMessage}</Modal.Body>
+        <Modal.Footer>
+          <Button variant="primary" onClick={ErrorClose}>
+            확인
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 }
